5b10f966961bea253475e2a1be9b4fabb9904a8d
Danie1Le <danielle8262005@gmail.com>
2025-02-26 15:44:11 -0800

    Create JSON file to hold data and call it in .js file

--- HW/routes/orders.js
+++ HW/routes/orders.js
@@ -1,26 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const ordersData = require('../order.json');
+const orders = ordersData.orders;
 
 // get orders
 router.get('/', function(req, res, next) {
-  // sample data
-  const orders = [
-    { topping: "cherry", quantity: 2 },
-    { topping: "plain", quantity: 6 },
-    { topping: "chocolate", quantity: 3 }
-  ];
-  
   // Send the orders array as JSON
   res.json(orders);
 });
 
 router.post('/', function(req, res, next) {
-    const orders = [
-      { topping: "cherry", quantity: 2 },
-      { topping: "plain", quantity: 6 },
-      { topping: "chocolate", quantity: 3 }
-    ];
-    
     res.json(orders);
 });
 
